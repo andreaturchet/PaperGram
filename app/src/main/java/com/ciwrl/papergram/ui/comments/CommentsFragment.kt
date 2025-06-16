67e7400a6437f73a5377373fc685835115e6b769
Andrea Turchet <andre.turky@gmail.com>
2025-06-16 10:52:12 +0200

    fix in CommentsFragment for a problem that caused app fcs

--- app/src/main/java/com/ciwrl/papergram/ui/comments/CommentsFragment.kt
+++ app/src/main/java/com/ciwrl/papergram/ui/comments/CommentsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
@@ -53,7 +54,13 @@ class CommentsFragment : Fragment() {
 
     private fun setupRecyclerView(paperId: String) {
         commentsAdapter = CommentsAdapter { commentToReply ->
-            showAddCommentDialog(paperId, commentToReply.id.toInt(), commentToReply.userName)
+            val parentId = commentToReply.id.toIntOrNull()
+
+            if (parentId != null) {
+                showAddCommentDialog(paperId, parentId, commentToReply.userName)
+            } else {
+                Toast.makeText(requireContext(), "Impossibile rispondere a questo commento d'esempio", Toast.LENGTH_SHORT).show()
+            }
         }
         binding.recyclerViewComments.apply {
             adapter = commentsAdapter
